81b773e4fad3282ccf5d0234a4c0ea6dec4323ce
RubenMateus <ruben.mateus3@gmail.com>
2018-04-15 01:12:12 +0100

    Cervus20 - Added eslint config

--- Cervus.Web/webpack.config.js
+++ Cervus.Web/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
+
 const bundleOutputDir = './wwwroot/dist';
 
 module.exports = (env) => {
@@ -36,8 +37,8 @@ module.exports = (env) => {
             // Plugins that apply in development builds only
             new webpack.SourceMapDevToolPlugin({
                 filename: '[file].map', // Remove this line if you prefer inline source maps
-                moduleFilenameTemplate: path.relative(bundleOutputDir, '[resourcePath]') // Point sourcemap entries to the original file locations on disk
-            })
+                moduleFilenameTemplate: path.relative(bundleOutputDir, '[resourcePath]'), // Point sourcemap entries to the original file locations on disk
+            }),
         ] : [
             // Plugins that apply in production builds only
             new webpack.optimize.UglifyJsPlugin(),
